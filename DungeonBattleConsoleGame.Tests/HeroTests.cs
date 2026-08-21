ba62aa89d418496b68f9b17e510f6c71c5d4fd5c
Makcim Blinda <114382903+TheSKBRook@users.noreply.github.com>
2026-08-21 10:33:07 +0300

    Add Hero healing unit tests

---
+++ DungeonBattleConsoleGame.Tests/HeroTests.cs
@@ -0,0 +1,39 @@
+﻿using DungeonBattleConsoleGame.Models.Characters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DungeonBattleConsoleGame.Tests
+{
+    [TestClass]
+    public class HeroTests
+    {
+        [TestMethod]
+        public void Heal_WhenHeroIsDamaged_RestoresHealth()
+        {
+            // Arrange
+            Hero hero = new Hero("Тестовий герой");
+            hero.TakeDamage(30);
+
+            // Act
+            int restoredHealth = hero.Heal(10);
+
+            // Assert
+            Assert.AreEqual(10, restoredHealth);
+            Assert.AreEqual(80, hero.Health);
+        }
+
+        [TestMethod]
+        public void Heal_WhenHealingExceedsMaxHealth_RestoresOnlyMissingHealth()
+        {
+            // Arrange
+            Hero hero = new Hero("Тестовий герой");
+            hero.TakeDamage(5);
+
+            // Act
+            int restoredHealth = hero.Heal(20);
+
+            // Assert
+            Assert.AreEqual(5, restoredHealth);
+            Assert.AreEqual(hero.MaxHealth, hero.Health);
+        }
+    }
+}
